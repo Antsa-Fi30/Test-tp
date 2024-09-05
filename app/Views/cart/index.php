05f3dd70d4b485ee570f2fd7c5934bc4cf4052e7
Antsa-Fi30 <antsafiononana30@gmail.com>
2024-09-05 00:27:46 +0000

    Adding Cart

--- app/Views/cart/index.php
+++ app/Views/cart/index.php
@@ -14,31 +14,26 @@
     <table class="table table-dark">
         <thead>
             <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col">id</th>
+                <th scope="col">Book</th>
+                <th scope="col">Client</th>
+                <th scope="col">Date of Borrowing</th>
+                <th scope="col">Date of returnings</th>
+                <th scope="col">Action</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
-            </tr>
+            <?php foreach ($carts as $cart): ?>
+                <tr>
+                    <th><?= $cart['id'] ?></th>
+                    <td><?= $cart['books_id'] ?></td>
+                    <td><?= $cart['client'] ?></td>
+                    <td><?= $cart['borrowed_at'] ?></td>
+                    <td><?= $cart['return_at'] ?></td>
+                    <td> <a href="/cart/delete/<?= $cart['id'] ?>" class="btn btn-outline-danger" type="button">Delete</a> </td>
+                </tr>
+
+            <?php endforeach; ?>
         </tbody>
     </table>
 </div>
